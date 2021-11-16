196955e76b5e77117d5a65f0d946e48c898e81e6
rshnloc <tiwariraushan71@gmail.com>
2021-11-16 18:18:14 +0530

    sfs
    dfs

--- src/components/TVChartContainer/api/pairs.js
+++ src/components/TVChartContainer/api/pairs.js
@@ -1,9 +1,44 @@
 export default {
   "BTC/USD": "Coinbase",
   "ETH/USD": "Coinbase",
-  "XLM/BTC": "Bitfinex",
-  "XLM/ETH": "Bitfinex",
   "XLM/USD": "Bitfinex",
+  "LTC/USD": "Coinbase",
+  "ETC/USD": "EXMO",
+  "ADA/USD": "Coinbase",
+  "SOL/USD": "Coinbase",
+  'NSE':'APOLLOHOSP',
+  "LINK/USD": "Coinbase",
+  "OMG/USD": "Coinbase",
+  "MATIC/USD": "Coinbase",
+  "ATOM/USD": "Coinbase",
+  "JASMY/USD": "Coinbase",
+  "DOGE/USD": "Coinbase",
+  "DOT/USD": "Coinbase",
+  "BCH/USD": "Coinbase",
+  "XTZ/USD": "Coinbase",
+  "OGN/USD": "Coinbase",
+  "CRO/USD": "Coinbase",
+  "ENJ/USD": "Coinbase",
+  "BAT/USD": "Coinbase",
+  "ZEC/USD": "Coinbase",
+  "EOS/USD": "Coinbase",
+  "QNT/USD": "Coinbase",
+  "UNI/USD": "Coinbase",
+  "XRP/USD": "Bitfinex",
+  "VET/USD": "Bitfinex",
+  "BTT/USD": "Bitfinex",
+  "NEO/USD": "Bitfinex",
+  "XVG/USD": "Bitfinex",
+  "BSV/USD": "Bitfinex",
+  "TRX/USD": "Bitfinex",
+  "ZIL/USD": "Bitfinex",
+  "XMR/USD": "Bitfinex",
+  "DASH/USD": "Bitfinex",
+  "SMART/USD": "EXMO",
+  "XEM/USD": "EXMO",
+  "WAVES/USD": "EXMO",
+  "ATOM/USD": "EXMO",
+  "QTUM/USD": "EXMO",
 }
 
 export const data = [
@@ -22,24 +57,263 @@ export const data = [
 		type: 'type'
 	},
 	{
-		symbol: 'XLM/BTC',
-		full_name: 'XLM/BTC',
-		description: 'description',
-		exchange: 'Bitfinex',
-		type: 'type'
-	},
-	{
-		symbol: 'XLM/ETH',
-		full_name: 'XLM/ETH',
-		description: 'description',
-		exchange: 'Bitfinex',
-		type: 'type'
-	},
-	{
 		symbol: 'XLM/USD',
 		full_name: 'XLM/USD',
 		description: 'description',
 		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+  symbol: 'LTC/USD',
+  full_name: 'LTC/USD',
+  description: 'description',
+  exchange: 'Coinbase',
+  type: 'type'
+  },
+	{
+  symbol: 'ETC/USD',
+  full_name: 'ETC/USD',
+  description: 'description',
+  exchange: 'EXMO',
+  type: 'type'
+},
+	{
+		symbol: 'ADA/USD',
+		full_name: 'ADA/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'SOL/USD',
+		full_name: 'SOL/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'LINK/USD',
+		full_name: 'LINK/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'OMG/USD',
+		full_name: 'OMG/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'MATIC/USD',
+		full_name: 'MATIC/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'ATOM/USD',
+		full_name: 'ATOM/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'JASMY/USD',
+		full_name: 'JASMY/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'DOGE/USD',
+		full_name: 'DOGE/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'DOT/USD',
+		full_name: 'DOT/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'BCH/USD',
+		full_name: 'BCH/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'XTZ/USD',
+		full_name: 'XTZ/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'OGN/USD',
+		full_name: 'OGN/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'CRO/USD',
+		full_name: 'CRO/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'ENJ/USD',
+		full_name: 'ENJ/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'BAT/USD',
+		full_name: 'BAT/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'ZEC/USD',
+		full_name: 'ZEC/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'EOS/USD',
+		full_name: 'EOS/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'QNT/USD',
+		full_name: 'QNT/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'UNI/USD',
+		full_name: 'UNI/USD',
+		description: 'description',
+		exchange: 'Coinbase',
+		type: 'type'
+	},
+	{
+		symbol: 'XRP/USD',
+		full_name: 'XRP/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'VET/USD',
+		full_name: 'VET/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'BTT/USD',
+		full_name: 'BTT/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'NEO/USD',
+		full_name: 'NEO/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'XVG/USD',
+		full_name: 'XVG/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'BSV/USD',
+		full_name: 'BSV/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'TRX/USD',
+		full_name: 'TRX/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'ZIL/USD',
+		full_name: 'ZIL/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'XMR/USD',
+		full_name: 'XMR/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'DASH/USD',
+		full_name: 'DASH/USD',
+		description: 'description',
+		exchange: 'Bitfinex',
+		type: 'type'
+	},
+	{
+		symbol: 'SMART/USD',
+		full_name: 'SMART/USD',
+		description: 'description',
+		exchange: 'EXMO',
+		type: 'type'
+	},
+	{
+		symbol: 'XEM/USD',
+		full_name: 'XEM/USD',
+		description: 'description',
+		exchange: 'EXMO',
+		type: 'type'
+	},
+	{
+		symbol: 'WAVES/USD',
+		full_name: 'WAVES/USD',
+		description: 'description',
+		exchange: 'EXMO',
+		type: 'type'
+	},
+	{
+		symbol: 'ATOM/USD',
+		full_name: 'ATOM/USD',
+		description: 'description',
+		exchange: 'EXMO',
+		type: 'type'
+	}
+	,
+	{
+		symbol: 'QTUM/USD',
+		full_name: 'QTUM/USD',
+		description: 'description',
+		exchange: 'EXMO',
 		type: 'type'
 	}
 ]
